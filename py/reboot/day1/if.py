b5fc03ebe1ccf977b8e085aea361e46101a78e91
Geng Jie <gengjie@outlook.com>
2015-08-04 12:46:21 +0800

    add str.py

--- py/reboot/day1/if.py
+++ py/reboot/day1/if.py
@@ -4,7 +4,7 @@ a=raw_input('Input a num: ')
 
 
 if str.isdigit(a):
-    print '\033[32;1mNumber : %d\033[0m' % int(a)
+    print '\033[32;1mNumber : %s\033[0m' % a
 else:
     print "\033[31;1m%s Not a number!\033[0m" % a
 
