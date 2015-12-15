88d85586cedce7e9dfef0279eeafba74ff719fc2
Geng Jie <gengjie@outlook.com>
2015-12-15 17:12:08 +0800

    add *.py

---
+++ py/sys/reference.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#Filename: reference.py
+
+print 'Simple Assignment'
+shoplist=['apple','mango','carrot','banana']
+mylist=shoplist
+
+del shoplist[0]
+
+print 'shoplist is ',shoplist
+print 'mylist is',mylist
+
+print 'Copy by making a full slice'
+mylist=shoplist[:]
+del mylist[0]
+
+print 'shop list is',shoplist
+print 'mylist is',mylist
---
+++ py/sys/str_methods.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#Filename:str_methods.py
+
+name='Swaroop'
+
+if name.startswith ('Swa'):
+	print 'Yes, the string start with "Swa"'
+
+if 'a' in name:
+	print 'Yes, it contains the string "a"'
+
+if name.find('war') != -1:
+	print 'Yes, it contains the string "war"'
+
+delimiter='_*_'
+mylist=['Brazil','Russia','India','China']
+
+print delimiter.join(mylist)
